e761ccac62474f6b34ed64b17c0bb7ed61ee2ac7
akabishau <aleksey.kabishau@icloud.com>
2024-10-28 12:28:19 -0400

    clean up chat page and add comments about progress

--- WORK.md
+++ WORK.md
@@ -14,3 +14,8 @@
 - added setUser action to current store to mutate the "user" state and use it after successful login response
 - updated fetching user info to the account page (researched the component lifecycle hook available - onMounted for simplicity)
 - added logout functionality to the account page
+- decided to refactor networking code to the api service - this cleaned up components
+- implemented global auth middleware after researching the topic - this is simple version, since only one middleware is added and it's global. Had some minor issues with the redirect loop to login during first implementation
+## Settings Modal and Chat Paste
+- added basic version of the modal settings component - had to do research on modals in general and ways to implement them; after a couple of iterations ended up with version that follows vue.js official examples using teleport and transition functionality
+- before implementing basic prevent pasting functionality had to research how to attach event listener to element and ClipboardEvent functionality in general
--- pages/chat.vue
+++ pages/chat.vue
@@ -4,8 +4,8 @@
         <div class="max-w-2xl w-full mx-auto py-10 px-4">
             <h1 class="font-bold text-4xl mb-6">New chat</h1>
             <textarea v-model="message" class="w-full min-h-44 rounded border resize-none p-2"
-                placeholder="Enter your chat here"
-                @paste="handlePaste">
+                :placeholder="placeholderText"
+                @paste="handlePaste"> <!-- js event(paste) is passed automatically as first arg -->
             </textarea>
             <button @click="sendChat()"
                 class="font-bold w-full px-4 py-2 bg-gray-100 hover:bg-gray-200 transition-colors rounded-b-lg">
@@ -15,20 +15,6 @@
     </div>
 </template>
 
-<!-- <script setup lang="ts">
-
-// TODO: prevent the user from pasting into the textarea if the enablePaste setting is disabled
-
-const message = ref('');
-
-function sendChat() {
-    console.log('SENDING CHAT');
-    message.value = '';
-    // don't worry about changing the functionality for "sendChat"
-}
-</script> -->
-
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useStore } from '~/stores/index';
@@ -36,13 +22,20 @@ import { useStore } from '~/stores/index';
 const message = ref('');
 const store = useStore();
 
-// Function to handle paste events
+
+// dynamic placeholder text based on user settings
+const placeholderText = computed(() =>
+  store.user?.settings.enablePaste === false
+    ? 'Ask here you question 💬 (No pasting, please 🙅‍♂️)'
+    : 'Chat with us! 💬'
+);
+
 function handlePaste(event: ClipboardEvent) {
+  event.preventDefault();
+
   // Check if paste is disabled in user settings
   if (store.user?.settings.enablePaste === false) {
-    // Prevent the paste action
     event.preventDefault();
-    alert("Pasting text is disabled.");
   }
 }
 
